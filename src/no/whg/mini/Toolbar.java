c3a13f65dce321197e2e2721d020727eb54fd493
Adhenedhel <peerandreas@live.com>
2013-10-16 16:39:20 +0200

    Added all image buttons

--- src/no/whg/mini/Toolbar.java
+++ src/no/whg/mini/Toolbar.java
@@ -18,23 +18,64 @@ public class Toolbar extends JToolBar {
 	public Toolbar()
 	{
 		JToolBar toolBar = new JToolBar();
-		toolBar.setPreferredSize(new Dimension(400,50));
+		toolBar.setMinimumSize(new Dimension(400, 50));
+		toolBar.setMaximumSize(new Dimension(400,80));
 		setFloatable(false);
 		setRollover(true);
-		JButton newButton = null;
+		setAlignmentY(TOP_ALIGNMENT);
+		JButton newButton = new JButton();
 		
 		ImageIcon newImage = new ImageIcon("gbleditor_icons/NEW.GIF");
 		newButton = new JButton(newImage);
-		newButton.setMinimumSize(new Dimension(20,20));
-		newButton.setMaximumSize(new Dimension(40,40));
 		newButton.setToolTipText("Create a new layout");
 		add(newButton);
 		
+		
+		
+		ImageIcon newOpenImage = new ImageIcon("gbleditor_icons/OPENDOC.GIF");
+		newButton = new JButton(newOpenImage);	
+		newButton.setToolTipText("Open document");
+		add(newButton);
+		
+		ImageIcon newSaveImage = new ImageIcon("gbleditor_icons/SAVE.GIF");
+		newButton = new JButton(newSaveImage);
+		newButton.setToolTipText("Save");
+		add(newButton);
+		
+		addSeparator();
+		
+		ImageIcon newSaveJavaImage = new ImageIcon("gbleditor_icons/SAVEJAVA.GIF");
+		newButton = new JButton(newSaveJavaImage);
+		newButton.setToolTipText("Generate the code");
+		add(newButton);
+		
+		ImageIcon newExecuteImage = new ImageIcon("gbleditor_icons/ExecuteProject.GIF");
+		newButton = new JButton(newExecuteImage);
+		newButton.setToolTipText("Run the program");
+		add(newButton);
+		
+		addSeparator();
+		
 		ImageIcon newRowImage = new ImageIcon("gbleditor_icons/NEWROW.GIF");
 		newButton = new JButton(newRowImage);
-		newButton.setMinimumSize(new Dimension(20,20));
-		newButton.setMaximumSize(new Dimension(40,40));
 		newButton.setToolTipText("Add a new row");
+		add(newButton);
+		
+		ImageIcon newRowDownImage = new ImageIcon("gbleditor_icons/MoveRowDown.GIF");
+		newButton = new JButton(newRowDownImage);
+		newButton.setToolTipText("Move row down");
+		add(newButton);
+		
+		ImageIcon newRowUpImage = new ImageIcon("gbleditor_icons/MoveRowUp.GIF");
+		newButton = new JButton(newRowUpImage);
+		newButton.setToolTipText("Move row up");
+		add(newButton);
+		
+		addSeparator();
+		
+		ImageIcon newHelpImage = new ImageIcon("gbleditor_icons/HELP.GIF");
+		newButton = new JButton(newHelpImage);
+		newButton.setToolTipText("Help");
 		add(newButton);
 	}
 	
