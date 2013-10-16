1f3130fb5ae8bf39cd523f6df11a89a6a591a052
Adhenedhel <peerandreas@live.com>
2013-10-16 18:00:12 +0200

    Added all menuitems to menubar

--- src/no/whg/mini/Menubar.java
+++ src/no/whg/mini/Menubar.java
@@ -12,15 +12,15 @@ public class Menubar extends JMenuBar
 {
 	public Menubar()
 	{
-		JMenuBar menuBar = new JMenuBar();
-		JMenu menu;
-		JMenuItem menuItem;
+		JMenuBar menuBar = new JMenuBar();	//the menubar that holds the menu
+		JMenu menu;	//an object of JMenu
+		JMenuItem menuItem;	//an object of JMenuItem
 		
-		menu = new JMenu("File");
-		add(menu);
+		menu = new JMenu("File");	//create a menu named file
+		add(menu); //add the menu too the menubar
 		
-		menuItem = new JMenuItem("New", new ImageIcon("gbleditor_icons/NEW.GIF"));
-		menu.add(menuItem);
+		menuItem = new JMenuItem("New", new ImageIcon("gbleditor_icons/NEW.GIF"));	//create an item to the menu with image
+		menu.add(menuItem);	//add the item to the menu
 		
 		menuItem = new JMenuItem("Load", new ImageIcon("gbleditor_icons/OPENDOC.GIF"));
 		menu.add(menuItem);
@@ -31,14 +31,38 @@ public class Menubar extends JMenuBar
 		menuItem = new JMenuItem("Save As..", new ImageIcon("gbleditor_icons/SAVE.GIF"));
 		menu.add(menuItem);
 		
+		menu.addSeparator();	//adds a line separator to the menu
+		
+		menuItem = new JMenuItem("Preview");	//create an item without the image
+		menu.add(menuItem);	//add the item to the menu
+		
+		menuItem = new JMenuItem("Generate code", new ImageIcon("gbleditor_icons/SAVEJAVA.GIF"));
+		menu.add(menuItem);
+		
 		menu.addSeparator();
 		
-		menuItem = new JMenuItem("Preview");
+		menuItem = new JMenuItem("Exit");
+		menu.add(menuItem);
 		
-		menu = new JMenu("Edit");
+		menu = new JMenu("Edit");	//creates a new menu next to the first
+		add(menu);	//add it to the JMenuBar
 		
+		menuItem = new JMenuItem("Add new row", new ImageIcon("gbleditor_icons/NEWROW.GIF"));
+		menu.add(menuItem);
 		
+		menu.addSeparator();
 		
+		menuItem = new JMenuItem("Preferences");
+		menu.add(menuItem);
+		
+		menu = new JMenu("Help");	//creates a new menu next to the two others
+		add(menu);	//add the menu to JMenuBar
+		
+		menuItem = new JMenuItem("Generate code", new ImageIcon("gbleditor_icons/HELP.GIF"));
+		menu.add(menuItem);
+		
+		menuItem = new JMenuItem("About");
+		menu.add(menuItem);
 		
 	}
 
