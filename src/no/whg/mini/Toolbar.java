47fd967b754986a5522d4ce2100f2b618e127d26
Adhenedhel <peerandreas@live.com>
2013-10-16 16:09:15 +0200

    Added tooltip to current buttons

--- src/no/whg/mini/Toolbar.java
+++ src/no/whg/mini/Toolbar.java
@@ -7,7 +7,6 @@ import java.awt.*;
 import javax.swing.ImageIcon;
 import javax.swing.JToolBar;
 import javax.swing.JButton;
-
 /**
  * Class that handles the toolbar of the program, save buttons etc
  * @author Fredrik, Peer
@@ -18,18 +17,21 @@ public class Toolbar extends JToolBar {
 	
 	public Toolbar()
 	{
-		
 		JToolBar toolBar = new JToolBar();
+		toolBar.setPreferredSize(new Dimension(400,50));
+		setFloatable(false);
+		setRollover(true);
+		JButton newButton = null;
 		
-		setSize(400,30);
 		ImageIcon newImage = new ImageIcon("NEW.GIF");
-		JButton newButton = new JButton(newImage);
-		newButton.setSize(50,50);
+		newButton = new JButton(newImage);
+		newButton.setPreferredSize(new Dimension(25,25));
+		newButton.setToolTipText("Create a new layout");
 		add(newButton);
 		
 		ImageIcon newRowImage = new ImageIcon("NEWROW.GIF");
 		newButton = new JButton(newRowImage);
-		newButton.setSize(50,50);
+		newButton.setToolTipText("Add a new row");
 		
 		
 		add(newButton);
